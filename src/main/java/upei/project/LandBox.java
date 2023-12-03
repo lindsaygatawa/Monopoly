e13e63c147238d7a50072d372a73266604bce4a3
Lindsay Gatawa <lindsaygatawa82@gmail.com>
2023-12-03 14:08:14 -0400

    added methods and created the utility class

--- src/main/java/upei/project/LandBox.java
+++ src/main/java/upei/project/LandBox.java
@@ -5,14 +5,7 @@ import upei.project.NewBox;
 public class LandBox extends NewBox {
 
 
-    public  String color;
-    public int boxPrice;
 
-    public void setRent(int rent) {
-        this.rent = rent;
-    }
-
-    public int rent;
 
     public Property property;
 
@@ -20,12 +13,9 @@ public class LandBox extends NewBox {
 
 
 
-    public LandBox(String boxName, int boxId,Player ownedBy,int l_boxPrice, int l_rent, String l_color) {
-        super(boxName, boxId, ownedBy);
+    public LandBox(String boxName, int boxId,Player ownedBy,int rent,  int l_boxPrice, String l_color) {
+        super(boxName, boxId, ownedBy, rent,l_boxPrice, l_color);
         super.boxType = "land";
-        boxPrice = l_boxPrice;
-        rent = l_rent;
-        color = l_color;
 
     }
 
--- src/main/java/upei/project/NewBox.java
+++ src/main/java/upei/project/NewBox.java
@@ -4,13 +4,19 @@ abstract class NewBox {
     String boxName;
     int boxId;
     String boxType;
-
     Player ownedBy;
+    public int rent;
+    public String color;
+    public int boxPrice;
 
-    public NewBox( String boxName,int boxId, Player ownedBy){
+
+    public NewBox( String boxName,int boxId, Player ownedBy,int rent,int boxPrice,String color){
         this.ownedBy = ownedBy;
         this.boxName = boxName;
         this.boxId = boxId;
+        this.rent = rent;
+        this.color = color;
+        this.boxPrice = boxPrice;
     }
 
     public NewBox(String boxName, int boxId) {
@@ -26,4 +32,14 @@ abstract class NewBox {
     public void setOwnedBy(Player ownedBy) {
         this.ownedBy = ownedBy;
     }
+
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+
 }
