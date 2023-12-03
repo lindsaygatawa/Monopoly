c17dcd1e879c5a7dbf434bfa71937a92a1cae25f
layofaloseyi <odfaloseyi@upei.ca>
2023-12-02 22:36:56 -0400

    Merge remote-tracking branch 'origin/main'

--- src/main/java/upei/project/Actions.java
+++ src/main/java/upei/project/Actions.java
@@ -24,17 +24,17 @@ public class Actions {
         newBoxes[2] = new CummunityBox("Community Chest",3);
         newBoxes[3] = new LandBox("Baltic Avenue",4,0,90,"Brown");
         newBoxes[4] = new Box("Income Tax",5,0,0,150);
-        newBoxes[5] = new Utility("Reading Railroad",6,0,200/);
+        newBoxes[5] = new Utility("Reading Railroad",6,0,200);
         newBoxes[6] = new LandBox("Oriental Avenue",7,0,120,"Light Blue");
         newBoxes[7] = new ChanceBox("Chance",8);
         newBoxes[8] = new LandBox("Vermont Avenue",9,0,130,"Light Blue");
         newBoxes[9] = new LandBox("Connecticut Avenue",10,0,250, "Light Blue");
         newBoxes[10] = new Box("Jail / Just Visiting",11,0,0,);
         newBoxes[11] = new LandBox("St. Charles Place",12,0,140,"Pink");
-        newBoxes[12] = new Utility("Electric company",13,0,140,"");
+        newBoxes[12] = new Utility("Electric company",13,0,140);
         newBoxes[13] = new LandBox("States ave",14,0,140,"Pink");
         newBoxes[14] = new LandBox("Virgina ave",15,0,140,"Pink");
-        newBoxes[15] = new Utility("Pennsylvania rail road",16,0,140,"");
+        newBoxes[15] = new Utility("Pennsylvania rail road",16,0,140);
         newBoxes[16] = new LandBox("St. James Place",17,0,140,"Orange");
         newBoxes[17] = new CummunityBox("Community chest",18);
         newBoxes[18] = new LandBox("Tennessee ave",19,0,140,"Orange");
@@ -44,17 +44,17 @@ public class Actions {
         newBoxes[22] = new ChanceBox("Chance",23);
         newBoxes[23] = new LandBox("Indiana ave",24,0,140,"Red");
         newBoxes[24] = new LandBox("Illinois ave",25,0,140,"Red");
-        newBoxes[25] = new Utility("B & O Railroad",26,0,140,"");
+        newBoxes[25] = new Utility("B & O Railroad",26,0,140);
         newBoxes[26] = new LandBox("Atlantic ave",27,0,140,"Yellow");
         newBoxes[27] = new LandBox("Ventnor ave",28,0,140,"Yellow");
-        newBoxes[28] = new Utility("Water work",29,0,140,"");
+        newBoxes[28] = new Utility("Water work",29,0,140);
         newBoxes[29] = new LandBox("Mavin gardens",30,0,140,"Yellow");
         newBoxes[30] = new Box("Go to Jail",31,0,140,"");
         newBoxes[31] = new LandBox("Pacific ave",32,0,140,"Green");
         newBoxes[32] = new LandBox("North Carolina ave",33,0,140,"Green");
         newBoxes[33] = new CummunityBox("Community chest",34);
         newBoxes[34] = new LandBox("Pennsylvania ave",35,0,140,"Green");
-        newBoxes[35] = new Utility("Short line Railroad",36,0,140,"");
+        newBoxes[35] = new Utility("Short line Railroad",36,0,140);
         newBoxes[36] = new ChanceBox("Chance",37);
         newBoxes[37] = new LandBox("Park place",38,0,140,"Dark Blue");
         newBoxes[38] = new Box("Luxury tax",39,0,140,"");
@@ -80,7 +80,7 @@ public class Actions {
     // updates player position and gives player $200 if they pass GO
     public void moveFoward(Player player){
         int steps = player.currentPosition + total;
-        if(steps > 40) {
+        if(steps >= 40) {
             player.currentPosition = steps % 40;
             player.acceptMoney(200);
         }
@@ -89,4 +89,6 @@ public class Actions {
         }
 
     }
+
+    
 }
--- src/main/java/upei/project/Utility.java
+++ src/main/java/upei/project/Utility.java
@@ -4,13 +4,11 @@ public class Utility extends NewBox{
     public Player ownedBy;
 
     public int boxPrice;
-
-
-
-
+    public int Rent;
     public Property property;
 
     public Player getOwnedBy() {
+
         return ownedBy;
     }
 
@@ -21,10 +19,11 @@ public class Utility extends NewBox{
 
 
 
-    public Utility(String boxName, int boxId, int l_boxPrice, int l_rent, String l_color) {
+    public Utility(String boxName, int boxId, int l_boxPrice, int l_rent) {
         super(boxName, boxId);
         super.boxType = "Utility";
         boxPrice = l_boxPrice;
+        Rent = l_rent;
 
 
 
