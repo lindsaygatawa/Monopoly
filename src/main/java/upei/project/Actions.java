6d1e9014cfacfa660d58cd816e9c242686a099bb
Lindsay Gatawa <lindsaygatawa82@gmail.com>
2023-12-02 22:42:46 -0400

    added methods and created the utility class

--- src/main/java/upei/project/Actions.java
+++ src/main/java/upei/project/Actions.java
@@ -19,17 +19,17 @@ public class Actions {
 
 
 
-        newBoxes[0] = new Box("Go",1);
+        newBoxes[0] = new NoActions("Go",1);
         newBoxes[1] = new LandBox("Mediterranean Avenue",2,50,60, "Brown");
         newBoxes[2] = new CummunityBox("Community Chest",3);
         newBoxes[3] = new LandBox("Baltic Avenue",4,0,90,"Brown");
-        newBoxes[4] = new Box("Income Tax",5,0,0,150);
+        newBoxes[4] = new NoActions("Income Tax",5);
         newBoxes[5] = new Utility("Reading Railroad",6,0,200);
         newBoxes[6] = new LandBox("Oriental Avenue",7,0,120,"Light Blue");
         newBoxes[7] = new ChanceBox("Chance",8);
         newBoxes[8] = new LandBox("Vermont Avenue",9,0,130,"Light Blue");
         newBoxes[9] = new LandBox("Connecticut Avenue",10,0,250, "Light Blue");
-        newBoxes[10] = new Box("Jail / Just Visiting",11,0,0,);
+        newBoxes[10] = new NoActions("Jail / Just Visiting",11);
         newBoxes[11] = new LandBox("St. Charles Place",12,0,140,"Pink");
         newBoxes[12] = new Utility("Electric company",13,0,140);
         newBoxes[13] = new LandBox("States ave",14,0,140,"Pink");
@@ -39,7 +39,7 @@ public class Actions {
         newBoxes[17] = new CummunityBox("Community chest",18);
         newBoxes[18] = new LandBox("Tennessee ave",19,0,140,"Orange");
         newBoxes[19] = new LandBox("New York ave",20,0,140,"Orange");
-        newBoxes[20] = new Box("Free parking",21,0,140,"");
+        newBoxes[20] = new NoActions("Free parking",21);
         newBoxes[21] = new LandBox("Kenturkey ave",22,0,140,"Red");
         newBoxes[22] = new ChanceBox("Chance",23);
         newBoxes[23] = new LandBox("Indiana ave",24,0,140,"Red");
@@ -49,7 +49,7 @@ public class Actions {
         newBoxes[27] = new LandBox("Ventnor ave",28,0,140,"Yellow");
         newBoxes[28] = new Utility("Water work",29,0,140);
         newBoxes[29] = new LandBox("Mavin gardens",30,0,140,"Yellow");
-        newBoxes[30] = new Box("Go to Jail",31,0,140,"");
+        newBoxes[30] = new NoActions("Go to Jail",31);
         newBoxes[31] = new LandBox("Pacific ave",32,0,140,"Green");
         newBoxes[32] = new LandBox("North Carolina ave",33,0,140,"Green");
         newBoxes[33] = new CummunityBox("Community chest",34);
@@ -57,7 +57,7 @@ public class Actions {
         newBoxes[35] = new Utility("Short line Railroad",36,0,140);
         newBoxes[36] = new ChanceBox("Chance",37);
         newBoxes[37] = new LandBox("Park place",38,0,140,"Dark Blue");
-        newBoxes[38] = new Box("Luxury tax",39,0,140,"");
+        newBoxes[38] = new NoActions("Luxury tax",39);
         newBoxes[39] = new LandBox("Board walk",40,0,140,"Dark Blue");
 
 
@@ -66,6 +66,9 @@ public class Actions {
     public void GoToJail(Player currentPlayer){
         currentPlayer.currentPosition =
 
+    }
+    public void advanceToGo(Player currentPlayer){
+        currentPlayer.currentPosition =
     }
     //roll dice method
 
@@ -87,6 +90,8 @@ public class Actions {
         else{
             player.currentPosition = steps;
         }
+
+
 
     }
 
---
+++ src/main/java/upei/project/NoActions.java
@@ -0,0 +1,19 @@
+package upei.project;
+
+import java.util.ArrayList;
+
+public class NoActions extends NewBox {
+    String boxName;
+    int boxId;
+
+    ArrayList<NoActions> noActions = new ArrayList<>();
+
+
+
+    public NoActions(String boxNamee, int boxID){
+        super(boxNamee,boxID);
+        super.boxType = "No actions";
+
+
+    }
+}
