c5dd1d1e54f3fedf132019e29eacb39bde511668
Lindsay Gatawa <lindsaygatawa82@gmail.com>
2023-12-03 16:04:11 -0400

    made changes in main and strategy class

--- src/main/java/upei/project/CummunityBox.java
+++ src/main/java/upei/project/CummunityBox.java
@@ -5,5 +5,6 @@ public class CummunityBox extends NewBox{
     public CummunityBox(String boxName, int boxId) {
 
         super(boxName, boxId);
+        super.boxType = "Community chest box";
     }
 }
