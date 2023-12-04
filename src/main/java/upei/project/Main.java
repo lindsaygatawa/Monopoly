19b12e0bb738518d52c86dc673c7eea568eb4244
layofaloseyi <odfaloseyi@upei.ca>
2023-12-04 14:38:29 -0400

    Merge remote-tracking branch 'origin/main'

--- src/main/java/upei/project/Main.java
+++ src/main/java/upei/project/Main.java
@@ -1,11 +1,12 @@
 package upei.project;
+import upei.project.Game;
 import java.util.ArrayList;
 
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
 
         Strategy strategy1 = new Strategy(true,  true, false,false,false, true,true,true,false,true,true);
         Strategy strategy2 = new Strategy(false, false,true,true,true,false,false,false,true,false,false);
@@ -14,15 +15,32 @@ public class Main {
         Player player1 = new Player("Ayo",1, 1500, strategy1);
         Player player2 = new Player("Ben",2, 1500, strategy2);
         Player player3 = new Player("Jay",3,1500,strategy3);
+        Player player4 = new Player("Mike",4,1500,strategy1);
 
         ArrayList<Player> players = new ArrayList<>();
         players.add(player1);
         players.add(player2);
 
-        Game game = new Game();
+        Game game = new Game(players);
+
+
+        ArrayList<Player> players2 = new ArrayList<>();
+        players2.add(player1);
+        players2.add(player2);
+        players2.add(player3);
+
+        Game game2 = new Game(players2);
+
+
+        ArrayList<Player> players3 = new ArrayList<>();
+        players3.add(player1);
+        players3.add(player4);
+        players3.add(player3);
+
+
+        Game game3 = new Game(players3);
 
     }
 
-    public static class Game {
-    }
+
 }
