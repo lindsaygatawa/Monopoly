7d399c96f0dec0b5b4841c6a7401f30f99b65fd9
layofaloseyi <odfaloseyi@upei.ca>
2023-12-04 17:17:59 -0400

    Merge remote-tracking branch 'origin/main'

--- src/test/java/upei/project/MainTest.java
+++ src/test/java/upei/project/MainTest.java
@@ -32,46 +32,47 @@ public class MainTest {
     @Test
     public void testPlayerMovement() {
         actions.moveForward(player1);
-        assertEquals("Player should have moved" + actions.rollDiceAndPrintTotal() + "spaces" , player1.getCurrentPosition());
+        assertEquals(1 , player1.getCurrentPosition());
     }
+
 
     //Also tests the getCurrentPosition method
     @Test
     public void testGoToJailmethod(){
         actions.GoToJail(player1,box);
-        assertEquals("Player should have moved to Box 31",player1.getCurrentPosition());
+        assertEquals( 31,player1.getCurrentPosition());
     }
     @Test
     public void testAdvanceTo(){
         actions.advanceTo(player1,chance);
-        assertEquals("Player should have moved to Box 29",player1.getCurrentPosition());
+        assertEquals(29,player1.getCurrentPosition());
     }
     @Test
     public void testGetOwnedBy(){
-        assertEquals("should return"+ player1,boxx.getOwnedBy());
+        assertEquals(player1,boxx.getOwnedBy());
     }
     @Test
     public void testSetOwnedBy(){
         boxx.setOwnedBy(player1);
-        assertEquals("Should set Owner to " +player1.playerName,player1.playerName);
+        assertEquals(player1.playerName,player1.playerName);
 
     }
 
     @Test
     public boolean testPurchaseMethod(){
-        assertEquals("Should return True", player1.makePurchase(200));
+        assertEquals(true, player1.makePurchase(200));
         return player1.makePurchase(200);
     }
 
     @Test
     public void testacceptMoney(){
         player1.acceptMoney(100);
-        assertEquals("Player should now have a balance of $1600",player1.currentMoney);
+        assertEquals( 1600,player1.currentMoney);
     }
     @Test
     public void testPayRent(){
         player1.payRent(100);
-        assertEquals("Player Should now have a balance of $1400",player1.currentMoney);
+        assertEquals(1400,player1.currentMoney);
     }
 
 
